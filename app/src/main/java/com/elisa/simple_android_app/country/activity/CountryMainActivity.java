43f218d4e1b4fadd2c25605d80ff199753f5bffc
nSafari <nassersafari@gmail.com>
2018-04-22 08:35:38 +0430

    Refactor Activity package.

---
+++ app/src/main/java/com/elisa/simple_android_app/country/activity/CountryMainActivity.java
@@ -0,0 +1,15 @@
+package com.elisa.simple_android_app.country.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.elisa.simple_android_app.R;
+
+public class CountryMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
